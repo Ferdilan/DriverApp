b6aca0bee9047d2480972e7a379c363fa31fd226
Ferdilan <naddiraaf@gmail.com>
2026-02-13 13:46:47 +0700

    Update gradle mqtt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,14 +52,20 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    packaging {
+        resources {
+            excludes += "META-INF/INDEX.LIST"
+            excludes += "META-INF/io.netty.versions.properties"
+        }
+    }
 }
 
 dependencies {
     // Paho MQTT
     implementation("com.amazon.ion:ion-java:1.10.4")
-    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
-    implementation("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
     implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
+    implementation("com.hivemq:hivemq-mqtt-client:1.3.3")
 
     // Google Maps & Location
     implementation("com.google.android.gms:play-services-maps:18.2.0")
@@ -67,11 +73,17 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.google.maps.android:android-maps-utils:3.4.0")
 
+    implementation("androidx.annotation:annotation:1.7.1")
+    implementation("org.jetbrains:annotations:23.0.0")
+
     // Android Standard Libs
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.annotations)
+    implementation(libs.annotations)
+    implementation(libs.annotations)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
